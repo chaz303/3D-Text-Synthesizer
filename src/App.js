fde0e088c5e04f6e88e255ef7d78e98f166edcda
Chaz <charlesssumnerwilson@gmail.com>
2019-02-12 14:20:47 +0900

    near mvp

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "aframe";
+import "aframe-particle-system-component";
+import { Entity, Scene } from "aframe-react";
+import React from "react";
+import "./index.css";
+//import ReactDOM from "react-dom";
 
-class App extends Component {
+class VRScene extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "Zexty Ext"
+    };
+  }
+  updateInputValue(evt) {
+    this.setState({
+      inputValue: evt.target.value
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p>This is another edit.</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <input
+          type="text"
+          value={this.state.inputValue}
+          onChange={evt => this.updateInputValue(evt)}
+        />
+        <h1>{this.state.inputValue}</h1>
+        <div className="a-frame-scene-box">
+          <Scene embedded background="color: red">
+            <Entity
+              scale="2 2 2"
+              geometry="primitive: torusKnot"
+              position="0 6 -10"
+              material="color: magenta; metalness:1; roughness: 0.1; sphericalEnvMap: #sky;"
+            >
+              <a-animation
+                easing="linear"
+                attribute="rotation"
+                dur="10000"
+                to="0 0 360"
+                repeat="indefinite"
+              />
+            </Entity>
+            <Entity text="value: {this.state.inputValue}" size="100" />
+          </Scene>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+//ReactDOM.render(<VRScene/>, document.querySelector('#sceneContainer'));
+export default VRScene;
