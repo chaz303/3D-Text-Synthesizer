a1e8781137676352854eb3f95c3ddcc5b2e1154d
Chaz <charlesssumnerwilson@gmail.com>
2019-02-12 17:51:30 +0900

    mvp plus 3d

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "aframe";
 import "aframe-particle-system-component";
+import "aframe-text-geometry-component";
 import { Entity, Scene } from "aframe-react";
 import React from "react";
 import "./index.css";
@@ -10,9 +11,12 @@ class VRScene extends React.Component {
     super(props);
     this.state = {
       inputValue: "Zexty Ext",
-      backgroundHue: 360,
-      backgroundSat: 94,
-      backgroundLum: 70
+      textHue: 200,
+      textSat: 100,
+      textLum: 70,
+      textRotX: 0,
+      textRotY: 0,
+      textRotZ: 0
     };
   }
   updateInputValue(evt) {
@@ -20,19 +24,34 @@ class VRScene extends React.Component {
       inputValue: evt.target.value
     });
   }
-  updateBackgroundHue(evt) {
+  updateTextHue(evt) {
     this.setState({
-      backgroundHue: evt.target.value
+      textHue: evt.target.value
     });
   }
-  updateBackgroundSat(evt) {
+  updateTextSat(evt) {
     this.setState({
-      backgroundSat: evt.target.value
+      textSat: evt.target.value
     });
   }
-  updateBackgroundLum(evt) {
+  updateTextLum(evt) {
     this.setState({
-      backgroundLum: evt.target.value
+      textLum: evt.target.value
+    });
+  }
+  updateTextRotX(evt) {
+    this.setState({
+      textRotX: evt.target.value
+    });
+  }
+  updateTextRotY(evt) {
+    this.setState({
+      textRotY: evt.target.value
+    });
+  }
+  updateTextRotZ(evt) {
+    this.setState({
+      textRotZ: evt.target.value
     });
   }
   render() {
@@ -47,40 +66,71 @@ class VRScene extends React.Component {
           type="range"
           min="1"
           max="360"
-          class="slider"
+          className="slider"
           id="bgHue"
-          onChange={evt => this.updateBackgroundHue(evt)}
+          onChange={evt => this.updateTextHue(evt)}
         />
         <input
           type="range"
           min="1"
           max="100"
-          class="slider"
+          className="slider"
           id="bgSat"
-          onChange={evt => this.updateBackgroundSat(evt)}
+          onChange={evt => this.updateTextSat(evt)}
         />
         <input
           type="range"
           min="1"
           max="100"
-          class="slider"
+          className="slider"
           id="bgLum"
-          onChange={evt => this.updateBackgroundLum(evt)}
+          onChange={evt => this.updateTextLum(evt)}
+        />
+        <input
+          type="range"
+          min="-360"
+          max="360"
+          className="slider"
+          id="txtRotX"
+          onChange={evt => this.updateTextRotX(evt)}
+        />
+        <input
+          type="range"
+          min="-360"
+          max="360"
+          className="slider"
+          id="txtRotY"
+          onChange={evt => this.updateTextRotY(evt)}
+        />
+        <input
+          type="range"
+          min="-360"
+          max="360"
+          className="slider"
+          id="txtRotZ"
+          onChange={evt => this.updateTextRotZ(evt)}
         />
         <div className="a-frame-scene-box">
-          <Scene
-            embedded
-            background={`color: hsla(${this.state.backgroundHue}, ${
-              this.state.backgroundSat
-            }%, ${this.state.backgroundLum}%)`}
-          >
+          <Scene embedded background="color: black">
+            <a-assets>
+              <a-asset-item
+                id="optimerBoldFont"
+                src="https://rawgit.com/mrdoob/three.js/dev/examples/fonts/optimer_bold.typeface.json"
+              />
+            </a-assets>
             <Entity
-              position="0 2 -6"
-              //text-geometry="bevelEnabled:true;bevelSize:0.1;bevelThickness:0.1;curveSegments:1;font:fonts/exo2Black.typeface.json;height:0.5;size:1.5;value:VAPORWAVE"
-              material="color:pink;metalness:0.9;roughness:0.05;sphericalEnvMap:https://img.gs/bbdkhfbzkk/2048x1024,stretch/http://i.imgur.com/WMNH2OF.jpg"
-              text={`width: 20; scale: 2.00; color: black; align: center; value: ${
+              position="-8 2 -10"
+              rotation={`${this.state.textRotX} ${this.state.textRotY} ${
+                this.state.textRotZ
+              }`}
+              text-geometry={`bevelEnabled:true;bevelSize:0.05;bevelThickness:0.1;curveSegments:1;height:0.5;size:1.5;font: #optimerBoldFont;value:${
                 this.state.inputValue
               }`}
+              material={`color: hsla(${this.state.textHue}, ${
+                this.state.textSat
+              }%, ${
+                this.state.textLum
+              }%) metalness:1.0;roughness:0.05;sphericalEnvMap:https://img.gs/bbdkhfbzkk/2048x1024,stretch/http://i.imgur.com/WMNH2OF.jpg"`}
             />
           </Scene>
         </div>
